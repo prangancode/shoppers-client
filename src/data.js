80862a6e29b573dd0d3e69fa6ebf697fe334174e
prangancode <senprangan@gmail.com>
2021-12-19 20:55:40 +0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/13719118/2021/3/3/af74bcc3-40aa-42c8-8f79-cdeab57e51c71614752823017-Athena-Turquoise-Blue-top-with-power-shoulder-sleeve-detail--5.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+        cat: "woman",
+    },
+    {
+        id: 2,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12834502/2020/11/26/e50bd870-573d-4f6f-9ebc-938bda01edc31606382198225WomenBlackMultiColourPrintedKurti1.jpg",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+        cat: "tshirt"
+    },
+    {
+        id: 3,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/13842966/2021/4/3/2a390640-fa09-4571-bd1f-327ca1ed1a261617444711576-Sassafras-Brown--Red-Geometric-Printed-Georgette-Wrap-Crop-T-5.jpg",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+        cat: "jacket"
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/15193820/2021/8/19/a01320fc-cee6-485e-9ce4-747dd43104321629352782279RegularFitOxfordshirt3.jpg",
+        title: "SHIRT STYLE!",
+        cat: "tshirt"
+    },
+    {
+        id: 2,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/14550590/2021/12/14/0c7a0af6-bdfd-4167-8ba9-ea3ec4ae30611639482739235RoadsterMenGreenRegularFitSolidCasualShirtJacketsNauticaWome3.jpg",
+        title: "WOMEN LOVE",
+        cat: "woman"
+    },
+    {
+        id: 3,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/14473496/2021/11/25/6426a4c6-1ce0-4a2e-9ced-da4c79eef97a1637825537349-HERENOW-Women-Jackets-5411637825536985-1.jpg",
+        title: "LIGHT JACKETS",
+        cat: "jacket"
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10188369/2019/9/16/508d517e-08c7-4a20-bd8e-20acf07aa4c31568616476657-WROGN-Men-Tshirts-2611568616475829-1.jpg",
+    },
+    {
+        id: 2,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2308267/2018/1/29/11517216335188-Roadster-Men-Maroon-Printed-Round-Neck-T-shirt-5591517216335098-3.jpg",
+    },
+    {
+        id: 3,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/14790032/2021/11/10/1b926d61-cd49-4cac-a9c3-9dc6d6b36a6a1636536690631-ADIDAS-Men-Tshirts-4751636536690141-1.jpg",
+    },
+    {
+        id: 4,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/15508918/2021/10/6/b1732025-43d1-4413-9843-090cd40a6f591633518810684-Indo-Era-Women-Yellow-Floral-Yoke-Design-Regular-Kurta-with--1.jpg",
+    },
+    {
+        id: 5,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2477343/2018/2/22/11519301743689-Libas-Women-Green-Woven-Design-Pathani-Kurta-4531519301743438-1.jpg",
+    },
+    {
+        id: 6,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/productimage/2021/1/18/b432359e-e89e-4df5-98e9-91496457fa991610973124193-1.jpg",
+    },
+    {
+        id: 7,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/6832202/2018/11/14/4d643960-977b-47ee-9a17-3fea5bc8e0a81542180487677-WROGN-Men-Charcoal-Solid-Bomber-1241542180487542-3.jpg",
+    },
+    {
+        id: 8,
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/16181560/2021/11/20/f7d57f82-9fef-48ac-93da-de55efd4bda61637389437194TokyoTalkiesWomenBlackFloralLeatherLonglineTailoredJacket1.jpg",
+    },
+]
